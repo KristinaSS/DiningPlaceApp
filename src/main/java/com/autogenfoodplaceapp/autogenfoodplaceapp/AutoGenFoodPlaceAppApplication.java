5e52f09c0bea9a878fa162d595613e79e4a63152
kristina.stoyanova <kristina.stoyanova@musala.com>
2019-10-09 16:25:42 +0300

    Initial Commit

---
+++ src/main/java/com/autogenfoodplaceapp/autogenfoodplaceapp/AutoGenFoodPlaceAppApplication.java
@@ -0,0 +1,13 @@
+package com.autogenfoodplaceapp.autogenfoodplaceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGenFoodPlaceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoGenFoodPlaceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autogenfoodplaceapp/autogenfoodplaceapp/AutoGenFoodPlaceAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.autogenfoodplaceapp.autogenfoodplaceapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutoGenFoodPlaceAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
